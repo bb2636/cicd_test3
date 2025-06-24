2505d2e77074130c069ef23b60115652c2b0dd20
bb2636 <leesubin0322@gmail.com>
2025-06-24 15:02:19 +0900

    first commit

---
+++ src/main/java/com/example/cicd_test3/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.cicd_test3.controller;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainController {
+    public static void main(String[] args) {
+        SpringApplication.run(MainController.class, args);
+    }
+}
---
+++ src/test/java/com/example/cicd_test3/CicdTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_test3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
